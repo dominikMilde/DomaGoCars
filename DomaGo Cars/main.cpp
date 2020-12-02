e947cdd5d631767dce96d9f051b7678daef893e7
Filip Skrlec <filip.skrlec@fer.hr>
2020-12-02 23:05:41 +0100

    NN_test

--- DomaGo Cars/main.cpp
+++ DomaGo Cars/main.cpp
@@ -1,3 +1,40 @@
+// main.cpp 
+
+// don't forget to include out neural network 
+#include "NeuralNetwork.hpp" 
+#include <iostream>
+#include <istream>
+#include "NeuralNetwork.cpp"
+
+//... data generator code here 
+
+void genData(std::string filename)
+{
+    std::ofstream file1(filename + "-in");
+    std::ofstream file2(filename + "-out");
+    for (unsigned int r = 0; r < 1000; r++) {
+        Scalar x = rand() / Scalar(RAND_MAX);
+        Scalar y = rand() / Scalar(RAND_MAX);
+        file1 << x << ", " << y << std::endl;
+        file2 << 2 * x + 10 + y << std::endl;
+    }
+    file1.close();
+    file2.close();
+}
+
+typedef std::vector<RowVector*> data;
+int main()
+{
+    NeuralNetwork n({ 2, 3, 1 });
+    data in_dat, out_dat;
+    genData("test");
+    ReadCSV("test-in", in_dat);
+    ReadCSV("test-out", out_dat);
+    n.train(in_dat, out_dat);
+    return 0;
+}
+
+/*
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include "simulator.cpp"
@@ -102,3 +139,4 @@ int main()
     }
     return 0;
 }
+*/
