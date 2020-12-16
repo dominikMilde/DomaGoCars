11c46225de51b68e151cdaff042d02f431921947
Filip Skrlec <filip.skrlec@fer.hr>
2020-12-16 10:44:03 +0100

    Dodani senzori i tekstualni prikaz

--- DomaGo Cars/main.cpp
+++ DomaGo Cars/main.cpp
@@ -7,44 +7,136 @@
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(sf::VideoMode::getDesktopMode().width, sf::VideoMode::getDesktopMode().height), "SFML auti", sf::Style::Close | sf::Style::Titlebar);
-    //sf::View view(sf::Vector2f(0.0f, 0.0f), sf::Vector2f(512.0f, 512.0f));
+    //sf::RenderWindow window(sf::VideoMode(sf::VideoMode::getDesktopMode().width, sf::VideoMode::getDesktopMode().height), "SFML auti", sf::Style::Close | sf::Style::Titlebar);
+    ////sf::View view(sf::Vector2f(0.0f, 0.0f), sf::Vector2f(512.0f, 512.0f));
+    //sf::RectangleShape background(sf::Vector2f(sf::VideoMode::getDesktopMode().width, sf::VideoMode::getDesktopMode().height));
+    //sf::Texture backgroundTexture;
+    //backgroundTexture.loadFromFile("background.jpg");
+    //background.setTexture(&backgroundTexture);
+
+    //sf::CircleShape player(30.0f);
+    //player.setOrigin(18.0f, 45.0f);
+    //player.setScale(1.5, 1.2);
+    //player.setPosition(sf::VideoMode::getDesktopMode().width / 2, sf::VideoMode::getDesktopMode().height / 1.2);
+    //int x1 = player.getPosition().x;
+    //int y1 = player.getPosition().y;
+    ////player.rotate(90);
+    //sf::Texture playerTexture;
+    //playerTexture.loadFromFile("avatar.jpg");
+    //player.setTexture(&playerTexture);
+
+    //auto image = backgroundTexture.copyToImage(); //jer texture nema getPixel, dimenzije se smanjuju za 5/3
+
+    //std::cout << image.getSize().x << std::endl;
+    //std::cout << image.getSize().y << std::endl;
+
+    //sf::Vector2f vector = player.getPosition();
+    //simulator sim(vector.x, vector.y, image);
+    ////std::cout << sim.getTopBoundDistance(x1, y1, image) << std::endl;
+    ////std::cout << sim.getBotBoundDistance(x1, y1, image) << std::endl;
+    ////std::cout << sim.getLeftBoundDistance(x1, y1, image) << std::endl;
+    ////std::cout << sim.getRightBoundDistance(x1, y1, image) << std::endl;
+    ////std::cout << sim.getTopLeftBoundDistance(x1, y1, image) << std::endl;
+    ////std::cout << sim.getTopRightBoundDistance(x1, y1, image) << std::endl;
+    ////std::cout << sim.getBotLeftBoundDistance(x1, y1, image) << std::endl;
+    ////std::cout << sim.getBotRightBoundDistance(x1, y1, image) << std::endl;
+
+    //while (window.isOpen())
+    //{
+    //    sf::Event evnt;
+    //    while (window.pollEvent(evnt))
+    //    {
+    //        switch (evnt.type)
+    //        {
+    //        case sf::Event::Closed:
+    //            window.close();
+    //            break;
+    //        case sf::Event::Resized:
+    //            std::cout << "New window width: " << evnt.size.width << " New window height: " << evnt.size.height << std::endl;
+    //            break;
+    //        case sf::Event::TextEntered:
+    //            break;
+    //        }
+    //    }
+
+    //    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
+    //    {
+    //        sim.rotateLeft();
+    //    }
+    //    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D))
+    //    {
+    //        sim.rotateRight();
+    //    }
+    //    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W))
+    //    {
+    //        sim.gas();
+    //    }
+    //    else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::S))
+    //    {
+    //        sim.brake();
+    //    }
+    //    else
+    //    {
+    //        sim.idle();
+    //    }
+
+    //    sim.update(image);
+    //    player.setPosition(sim.getX(), sim.getY());
+    //    player.setRotation(sim.getAngle() * -1.0);
+    //    //std::cout << sim.getV() << std::endl;
+
+    //    window.clear();
+    //    window.draw(background);
+    //    window.draw(player);
+    //    window.display();
+
+    //    float koef = 1.666666666666666666666; //jer copyToImage smanjuje broj piksela
+
+    //    float x = player.getPosition().x;
+    //    float y = player.getPosition().y;
+
+    //    x /= koef;
+    //    y /= koef;
+
+    //    auto color1 = image.getPixel(x, y);
+    //    //auto color2 = image.getPixel(x, y + image.getSize().y);
+    //    //auto color3 = image.getPixel(x + image.getSize().x, y + image.getSize().y);
+
+    //    if (color1 == sf::Color::Black)
+    //    //|| color2 == sf::Color::Black || color3 == sf::Color::Black)
+    //    {
+    //        //player.setPosition(sf::VideoMode::getDesktopMode().width / 2, sf::VideoMode::getDesktopMode().height / 1.2);
+    //        sim.setX(sf::VideoMode::getDesktopMode().width / 2);
+    //        sim.setY(sf::VideoMode::getDesktopMode().height / 1.2);
+    //        sim.setAngle(0);
+    //        player.setRotation(0);
+    //        sim.setV(0);
+    //    }
+    //}
+    //return 0;
+
     sf::RectangleShape background(sf::Vector2f(sf::VideoMode::getDesktopMode().width, sf::VideoMode::getDesktopMode().height));
     sf::Texture backgroundTexture;
     backgroundTexture.loadFromFile("background.jpg");
     background.setTexture(&backgroundTexture);
 
     sf::CircleShape player(30.0f);
+    player.setOrigin(18.0f, 45.0f);
     player.setScale(1.5, 1.2);
     player.setPosition(sf::VideoMode::getDesktopMode().width / 2, sf::VideoMode::getDesktopMode().height / 1.2);
-    //player.rotate(90);
-    player.setOrigin(30.0f, 51.0f);
     sf::Texture playerTexture;
     playerTexture.loadFromFile("avatar.jpg");
     player.setTexture(&playerTexture);
 
     auto image = backgroundTexture.copyToImage(); //jer texture nema getPixel, dimenzije se smanjuju za 5/3
 
-    sf::Vector2f vector = player.getPosition();
-    simulator sim(vector.x, vector.y);
 
-    while (window.isOpen())
-    {
-        sf::Event evnt;
-        while (window.pollEvent(evnt))
-        {
-            switch (evnt.type)
-            {
-            case sf::Event::Closed:
-                window.close();
-                break;
-            case sf::Event::Resized:
-                std::cout << "New window width: " << evnt.size.width << " New window height: " << evnt.size.height << std::endl;
-                break;
-            case sf::Event::TextEntered:
-                break;
-            }
-        }
+    sf::Vector2f vector = player.getPosition();
+    simulator sim(vector.x, vector.y, image);
+
+    while (true) {
+
+
 
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A))
         {
@@ -67,15 +159,10 @@ int main()
             sim.idle();
         }
 
-        sim.update();
+        sim.update(image);
         player.setPosition(sim.getX(), sim.getY());
         player.setRotation(sim.getAngle() * -1.0);
         //std::cout << sim.getV() << std::endl;
-
-        window.clear();
-        window.draw(background);
-        window.draw(player);
-        window.display();
 
         float koef = 1.666666666666666666666; //jer copyToImage smanjuje broj piksela
 
@@ -90,7 +177,7 @@ int main()
         //auto color3 = image.getPixel(x + image.getSize().x, y + image.getSize().y);
 
         if (color1 == sf::Color::Black)
-     //|| color2 == sf::Color::Black || color3 == sf::Color::Black)
+            //|| color2 == sf::Color::Black || color3 == sf::Color::Black)
         {
             //player.setPosition(sf::VideoMode::getDesktopMode().width / 2, sf::VideoMode::getDesktopMode().height / 1.2);
             sim.setX(sf::VideoMode::getDesktopMode().width / 2);
@@ -98,7 +185,24 @@ int main()
             sim.setAngle(0);
             player.setRotation(0);
             sim.setV(0);
+            
+            for (int i = 0; i < 5; i++)
+            {
+                std::cout << "==========================================" << std::endl;
+            }
+            std::cout << "SUDAR" << std::endl;
+            for (int i = 0; i < 5; i++)
+            {
+                std::cout << "==========================================" << std::endl;
+            }
         }
+
+        std::cout << "Trenutna pozicija: " << sim.getX() << " " << sim.getY() << " Brzina: " << sim.getV() << std::endl;
+        std::cout << "Top: " << sim.getTopDistance() << " " <<" Bot: " << sim.getBotDistance() << std::endl;
+
     }
     return 0;
+
+
+
 }
--- DomaGo Cars/simulator.cpp
+++ DomaGo Cars/simulator.cpp
@@ -1,4 +1,6 @@
 #include <math.h>
+#include <cmath>
+#include <SFML/Graphics.hpp>
 
 #define PI 3.14159265
 #define GAS_ACC 0.001
@@ -13,28 +15,63 @@ private:
     double v;
     double angle;
     double acc;
+    int topDistance;
+    int botDistance;
+    int leftDistance;
+    int rightDistance;
+    int topLeftDistance;
+    int topRightDistance;
+    int botLeftDistance;
+    int botRightDistance;
 
 public:
-    simulator(double x1, double y1) {
+    simulator(double x1, double y1, sf::Image image) {
         x = x1;
         y = y1;
         v = 0.0;
         angle = 0.0;
         acc = 0.0;
+        topDistance = getTopBoundDistance(x1, y1 , image);
+        botDistance = getBotBoundDistance(x1, y1, image);
+        leftDistance = getLeftBoundDistance(x1, y1, image);
+        rightDistance = getRightBoundDistance(x1, y1, image);
+        topLeftDistance = getTopLeftBoundDistance(x1, y1, image);
+        topRightDistance = getTopRightBoundDistance(x1, y1, image);
+        botLeftDistance = getBotLeftBoundDistance(x1, y1, image);
+        botRightDistance = getBotRightBoundDistance(x1, y1, image);
     }
 
-    void update() {
+    void update(sf::Image image) {
         v = (v + acc * T) * DRAG_K;
         if (v < 0) v = 0;
         if (angle <= 0) { angle += 360; }
         x = x + cos(angle * PI / 180) * v * T;
         y = y - sin(angle * PI / 180) * v * T;
+        topDistance = getTopBoundDistance(x, y, image);
+        botDistance = getBotBoundDistance(x, y, image);
+        leftDistance = getLeftBoundDistance(x, y, image);
+        rightDistance = getRightBoundDistance(x, y, image);
+        topLeftDistance = getTopLeftBoundDistance(x, y, image);
+        topRightDistance = getTopRightBoundDistance(x, y, image);
+        botLeftDistance = getBotLeftBoundDistance(x, y, image);
+        botRightDistance = getBotRightBoundDistance(x, y, image);
+
     }
 
     double getAngle() { return angle; }
     double getV() { return v; }
     double getX() { return x; }
     double getY() { return y; }
+    int getTopDistance() { return topDistance; }
+    int getBotDistance() { return botDistance; }
+    int getLeftDistance() { return leftDistance; }
+    int getRightDistance() { return rightDistance; }
+    int getTopLeftDistance() { return topLeftDistance; }
+    int getTopRightDistance() { return topRightDistance; }
+    int getBotLeftDistance() { return botLeftDistance; }
+    int getBotRightDistance() { return botRightDistance; }
+
+
     void setV(double v) {
         this->v = v;
     }
@@ -61,5 +98,134 @@ public:
     }
     void idle() {
         acc = 0.0;
+    }
+
+    int getTopBoundDistance(int x, int y, sf::Image image) {
+        float koef = 1.666666666666666666666; //jer copyToImage smanjuje broj piksela
+        x /= koef;
+        y /= koef;
+        sf::Color color = image.getPixel(x, y);
+        int i = 1;
+        while (color != sf::Color::Black)
+        {
+            color = image.getPixel(x, y - i);
+            i += 1;
+        }
+
+        return (i - 1) * 1.666666666666666666666;
+    }
+
+    int getBotBoundDistance(int x, int y, sf::Image image) {
+        float koef = 1.666666666666666666666; //jer copyToImage smanjuje broj piksela
+        x /= koef;
+        y /= koef;
+        sf::Color color = image.getPixel(x, y);
+        int i = 1;
+        while (color != sf::Color::Black)
+        {
+            color = image.getPixel(x, y + i);
+            i += 1;
+        }
+
+        return (i - 1) * 1.666666666666666666666;
+    }
+
+
+    int getLeftBoundDistance(int x, int y, sf::Image image) {
+        float koef = 1.666666666666666666666; //jer copyToImage smanjuje broj piksela
+        x /= koef;
+        y /= koef;
+        sf::Color color = image.getPixel(x, y);
+        int i = 1;
+        while (color != sf::Color::Black)
+        {
+            color = image.getPixel(x - i, y);
+            i += 1;
+        }
+
+        return (i - 1) * 1.666666666666666666666;
+    }
+
+    int getRightBoundDistance(int x, int y, sf::Image image) {
+        float koef = 1.666666666666666666666; //jer copyToImage smanjuje broj piksela
+        x /= koef;
+        y /= koef;
+        sf::Color color = image.getPixel(x, y);
+        int i = 1;
+        while (color != sf::Color::Black)
+        {
+            color = image.getPixel(x + i, y);
+            i += 1;
+        }
+
+        return (i - 1) * 1.666666666666666666666;
+    }
+
+    int getTopLeftBoundDistance(int x, int y, sf::Image image) {
+        float koef = 1.666666666666666666666; //jer copyToImage smanjuje broj piksela
+        x /= koef;
+        y /= koef;
+        sf::Color color = image.getPixel(x, y);
+        int i = 1;
+        while (color != sf::Color::Black)
+        {
+            color = image.getPixel(x - i, y - i);
+            i += 1;
+        } 
+
+        i -= 1;
+
+        return sqrt(2 * pow(i, 2));
+    }
+
+    int getTopRightBoundDistance(int x, int y, sf::Image image) {
+        float koef = 1.666666666666666666666; //jer copyToImage smanjuje broj piksela
+        x /= koef;
+        y /= koef;
+        sf::Color color = image.getPixel(x, y);
+        int i = 1;
+        while (color != sf::Color::Black)
+        {
+            color = image.getPixel(x + i, y - i);
+            i += 1;
+        }
+
+        i -= 1;
+
+        return sqrt(2 * pow(i, 2));
+    }
+
+    int getBotLeftBoundDistance(int x, int y, sf::Image image) {
+        float koef = 1.666666666666666666666; //jer copyToImage smanjuje broj piksela
+        x /= koef;
+        y /= koef;
+        sf::Color color = image.getPixel(x, y);
+        int i = 1;
+        while (color != sf::Color::Black)
+        {
+            color = image.getPixel(x - i, y + i);
+            i += 1;
+        }
+
+        i -= 1;
+
+        return sqrt(2 * pow(i, 2));
+    }
+
+    int getBotRightBoundDistance(int x, int y, sf::Image image) {
+        float koef = 1.666666666666666666666; //jer copyToImage smanjuje broj piksela
+        x /= koef;
+        y /= koef;
+        sf::Color color = image.getPixel(x, y);
+        int i = 1;
+        while (color != sf::Color::Black)
+        {
+            color = image.getPixel(x + i, y + i);
+            i += 1;
+        }
+
+        i -= 1;
+
+        return sqrt(2 * pow(i, 2));
     }
 };
