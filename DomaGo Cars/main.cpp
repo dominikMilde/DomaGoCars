725d502be5fc70f2998988f31889b8ddbaed64bb
Filip Skrlec <filip.skrlec@fer.hr>
2020-12-22 18:32:28 +0100

    Dodan neuralnetwork

--- DomaGo Cars/main.cpp
+++ DomaGo Cars/main.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include "simulator.cpp"
 
+
 using namespace std;
 
 //auto se vozi sa w,s,a,d i ako dode do crne pozadine vraca se na pocetak
@@ -69,11 +70,10 @@ int run(const simulator& sim) {
 
 }
 
-int main()
+double evaluate()
 {
     int imageWidth = 1152;
     int imageHeight = 648;
-    sf::RenderWindow window(sf::VideoMode(imageWidth, imageHeight), "SFML auti", sf::Style::Close | sf::Style::Titlebar);
     sf::RectangleShape background(sf::Vector2f(imageWidth, imageHeight));
     sf::Texture backgroundTexture;
     backgroundTexture.loadFromFile("background.jpg");
@@ -84,36 +84,15 @@ int main()
     player.setScale(1.5, 1.2);
     player.setPosition(imageWidth / 2, imageHeight / 1.2);
     player.setRotation(0);
-    sf::Texture playerTexture;
-    playerTexture.loadFromFile("avatar.jpg");
-    player.setTexture(&playerTexture);
 
     auto image = backgroundTexture.copyToImage(); //jer texture nema getPixel, dimenzije se smanjuju za 5/3
-
-    std::cout << image.getSize().x << std::endl;
-    std::cout << image.getSize().y << std::endl;
 
     sf::Vector2f vector = player.getPosition();
     simulator sim(vector.x, vector.y, image);
     sim.setV(0.4);
 
-    while (window.isOpen())
+    while (true)
     {
-        sf::Event evnt;
-        while (window.pollEvent(evnt))
-        {
-            switch (evnt.type)
-            {
-            case sf::Event::Closed:
-                window.close();
-                break;
-            case sf::Event::Resized:
-                std::cout << "New window width: " << evnt.size.width << " New window height: " << evnt.size.height << std::endl;
-                break;
-            case sf::Event::TextEntered:
-                break;
-            }
-        }
 
         int akcija = run(sim);
         //cout << akcija << endl;
@@ -158,10 +137,6 @@ int main()
         player.setPosition(sim.getX(), sim.getY());
         player.setRotation(sim.getAngle() * -1.0);
 
-        window.clear();
-        window.draw(background);
-        window.draw(player);
-        window.display();
 
         float x = player.getPosition().x;
         float y = player.getPosition().y;
@@ -172,16 +147,7 @@ int main()
 
         if (color1 == sf::Color::Black)
         {
-            std::cout << "T: " << sim.getT() << std::endl;
-            std::cout << "Distance(in degrees): " << sim.getAngleDistance() << std::endl;
-            sim.setT(0);
-            sim.setX(imageWidth / 2);
-            sim.setY(imageHeight / 1.2);
-            sim.setAngle(0);
-            player.setRotation(0);
-            sim.setV(0);
-            sim.setAngleDistance(0.0f);
+            return sim.getAngleDistance() * sim.getAngleDistance() / sim.getT();
         }
     }
-    return 0;
 }
